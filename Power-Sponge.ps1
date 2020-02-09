89df380148df01290312780d1e108e36b25a9cec
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-02-09 20:23:06 +0000

    Update Power-Sponge.ps1

--- Power-Sponge.ps1
+++ Power-Sponge.ps1
@@ -1,1 +1,32 @@
+<#
+Script Name: Power-Sponge.ps1
+Author: Dark-Coffee
+Version: 0.0.2
+Updated: 2020-02-09
+Url: https://github.com/dark-coffee/Power-Sponge/
+#>
 
+<#
+.Description
+    [description]
+.Parameter SuperCaps
+    Increases the number of capital letters.
+.Parameter LessCaps
+    Decreases the number of capital letters.
+.Example
+    Power-Sponge
+.Example
+    Power-Sponge -SuperCaps
+.Example
+    Power-Sponge -LessCaps
+#>
+
+
+function Power-Sponge{
+
+    #Set Parameters
+    [Parameter(Mandatory=$false)][string]$SuperCaps,
+    [Parameter(Mandatory=$false)][string]$LessCaps
+
+    
+}
