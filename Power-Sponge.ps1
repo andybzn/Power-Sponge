4f78674c1ca78f8667b7be63c735623a64ce465c
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2020-02-10 11:07:03 +0000

    Update Power-Sponge.ps1

--- Power-Sponge.ps1
+++ Power-Sponge.ps1
@@ -1,24 +1,20 @@
 <#
 Script Name: Power-Sponge.ps1
 Author: Dark-Coffee
-Version: 0.0.2
-Updated: 2020-02-09
+Version: 1.0.0
+Updated: 2020-02-10
 Url: https://github.com/dark-coffee/Power-Sponge/
 #>
 
 <#
 .Description
-    [description]
+    takes your text, AnD MAkEs it liKE tHiS.
 .Parameter SuperCaps
     Increases the number of capital letters.
-.Parameter LessCaps
-    Decreases the number of capital letters.
 .Example
     Power-Sponge
 .Example
     Power-Sponge -SuperCaps
-.Example
-    Power-Sponge -LessCaps
 #>
 
 
@@ -26,8 +22,44 @@ function Power-Sponge{
 
     #Set Parameters
     param(
-        [Parameter(Mandatory=$false)][string]$SuperCaps,
-        [Parameter(Mandatory=$false)][string]$LessCaps
+        [Parameter(Mandatory=$false)][Switch]$SuperCaps
     )
     
+    #Initialise blank variables
+    $inputText=""
+    $inputArray=""
+
+    #Get text from clipboard and break into an array
+    $inputText=Get-Clipboard
+    [Array]$inputArray = $inputText.ToCharArray()
+
+    #Decide number of caps
+    if($SuperCaps){
+        $CapsAmount = 25
+    }else{
+        $CapsAmount = 50
+    }
+
+    #Capitalise at will
+    foreach ($item in $inputArray){
+        
+        #Force as string
+        [string]$Character = $item
+
+        if((0..100 | Get-Random) -le $CapsAmount){
+            $Character = $Character.ToLower()
+        }else{
+            $Character = $Character.ToUpper()
+        }
+        
+        #Append to Output Text
+        $outputtext = $outputtext + $Character
+    }
+
+        #Force Output to String
+        [String]$SpOnGeIfIeD = $outputtext.ToString();
+        
+        #Put to Host and Clipboard
+        $SpOnGeIfIeD | clip
+        $SpOnGeIfIeD
 }
